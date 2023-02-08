6f6d1b4639a52a1485785605915f1ea586e36255
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-09 00:06:58 +0530

    Navbar Menu

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
       </PersistGate>
     </Provider>
--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -1,5 +1,12 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default Home;
