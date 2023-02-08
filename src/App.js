28179ef987533ab68767acad9e622d29e044604b
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-08 22:07:36 +0530

    Color, Theme and Styling Setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { useMemo } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "scenes/home";
 import Login from "scenes/login";
 import Profile from "scenes/profile";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme/theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile/:userId" element={<Profile />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
