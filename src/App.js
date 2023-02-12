42f25e928b1c723828820f1ab99a41587f6235b9
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-12 14:11:28 +0530

    Sign Up, Sign In Pages and Forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "scenes/home";
 import Login from "scenes/login";
 import Profile from "scenes/profile";
@@ -11,15 +11,22 @@ import { themeSettings } from "theme/theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={isAuth ? <Home /> : <Navigate to="/login" />}
+          />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile/:userId" element={<Profile />} />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <Profile /> : <Navigate to="/login" />}
+          />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
