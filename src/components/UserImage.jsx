5b7a3a1672c30f923c1b96f988d1cdea6ed4770a
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-16 18:12:37 +0530

    Home Page and Widgets

---
+++ src/components/UserImage.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+const UserImage = ({ image, size = "60px" }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        src={`http://localhost:3001/assets/${image}`}
+        alt="profile"
+      />
+    </Box>
+  );
+};
+
+export default UserImage;
--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -1,10 +1,31 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const Home = () => {
+  const isNonMobile = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobile ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobile ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobile ? "42%" : undefined}
+          mt={isNonMobile ? undefined : "2rem"}
+        ></Box>
+        {isNonMobile && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
